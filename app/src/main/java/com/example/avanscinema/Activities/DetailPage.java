c898f4fe80d87e03a49137336997429cabe470be
Jelmer1220000 <jjp.verhoeven@student.avans.nl>
2022-04-02 19:40:51 +0200

    Detail page updated

--- app/src/main/java/com/example/avanscinema/Activities/DetailPage.java
+++ app/src/main/java/com/example/avanscinema/Activities/DetailPage.java
@@ -9,9 +9,16 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.avanscinema.Classes.Cast;
 import com.example.avanscinema.Classes.Movie;
+import com.example.avanscinema.Classes.Review;
+import com.example.avanscinema.JsonParsers.CastList;
+import com.example.avanscinema.JsonParsers.ReviewList;
 import com.example.avanscinema.R;
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class DetailPage extends AppCompatActivity {
@@ -26,7 +33,6 @@ public class DetailPage extends AppCompatActivity {
         Title = findViewById(R.id.title_movie);
         poster = findViewById(R.id.detail_movie);
 
-
         getIntentData();
 
 
@@ -35,9 +41,28 @@ public class DetailPage extends AppCompatActivity {
     private void getIntentData(){
         if (getIntent() !=null){
             Intent intent = getIntent();
-            Movie movie = (Movie)intent.getSerializableExtra("movie");
+           List<Cast> cast = retrieveCast(intent);
+           List<Review> reviews = getReviews(intent);
+            Movie movie = getMovie(intent);
+
             Title.setText(movie.getTitle());
             Picasso.get().load(movie.getLandscapeImage()).into(poster);
         }
+    }
+
+
+    private List<Cast> retrieveCast(Intent intent) {
+        CastList cast = (CastList) intent.getSerializableExtra("cast");
+        List<Cast> popes = cast.getCast();
+        return popes;
+    }
+    private List<Review> getReviews(Intent intent) {
+        ReviewList reviewlist = (ReviewList) intent.getSerializableExtra("reviews");
+        List<Review> reviews = reviewlist.getResults();
+        return reviews;
+    }
+    private Movie getMovie(Intent intent) {
+        Movie movie = (Movie)intent.getSerializableExtra("movie");
+        return movie;
     }
 }
