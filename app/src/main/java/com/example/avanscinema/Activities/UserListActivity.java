71d31ef9b271ce2292998117f7abd66c0b987398
Tristan Goossens <tt.goossens@student.avans.nl>
2022-04-04 15:10:23 +0200

    retrieve user lists from api and set adapter

--- app/src/main/java/com/example/avanscinema/Activities/UserListActivity.java
+++ app/src/main/java/com/example/avanscinema/Activities/UserListActivity.java
@@ -6,14 +6,17 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.avanscinema.API.ApiConnection;
+import com.example.avanscinema.API.ResponseListener;
+import com.example.avanscinema.API.UserListResponseListener;
+import com.example.avanscinema.Adapters.RecyclerAdapterUserListPage;
 import com.example.avanscinema.Classes.UserMovieList;
 import com.example.avanscinema.R;
 
 import java.util.List;
 
-public class UserListActivity extends AppCompatActivity {
+public class UserListActivity extends AppCompatActivity implements UserListResponseListener {
     private RecyclerView recyclerView;
-    private List<UserMovieList> movieList;
 
 
     @Override
@@ -24,9 +27,18 @@ public class UserListActivity extends AppCompatActivity {
         // Get recyclerview component and set a linear layout
         recyclerView = (RecyclerView) findViewById(R.id.user_list);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        ApiConnection api = new ApiConnection();
+        api.getUserMovieLists(this);
     }
 
-    private void setRecyclerViewAdapter(List<UserMovieList> userList) {
+    private void setRecyclerViewAdapter(List<UserMovieList> userMovieLists) {
+        RecyclerAdapterUserListPage userMovieListAdapter = new RecyclerAdapterUserListPage(this, userMovieLists);
+        recyclerView.setAdapter(userMovieListAdapter);
+    }
 
+    @Override
+    public void onUserMovieListResponse(List<UserMovieList> userMovieLists) {
+        setRecyclerViewAdapter(userMovieLists);
     }
 }
