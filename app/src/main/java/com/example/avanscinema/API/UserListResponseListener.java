a4185c8512eac9035c598935564302b6cdffe59a
Tristan Goossens <tt.goossens@student.avans.nl>
2022-04-05 15:19:44 +0200

    added user list detail response listener

--- app/src/main/java/com/example/avanscinema/API/UserListResponseListener.java
+++ app/src/main/java/com/example/avanscinema/API/UserListResponseListener.java
@@ -2,8 +2,6 @@ package com.example.avanscinema.API;
 
 import com.example.avanscinema.Classes.UserMovieList;
 
-import java.util.List;
-
 public interface UserListResponseListener {
-    public void onUserMovieListResponse(List<UserMovieList> userMovieLists);
+    public void onUserListResponse(UserMovieList list);
 }
